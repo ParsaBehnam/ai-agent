09363ae2b008b2720a485f60b8892673d7979639
ParsaBehnam <183300741+ParsaBehnam@users.noreply.github.com>
2026-03-26 20:06:35 +0300

    add user input functionality

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 import os
+import argparse
+
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
-    if api_key == None:
+    if not api_key:
         raise RuntimeError("could not connect to the api")
 
     client = genai.Client(api_key = api_key)
-    response = client.models.generate_content(model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
-    if response.usage_metadata == None:
+    response = client.models.generate_content(
+        model='gemini-2.5-flash',
+        contents= messages)
+    
+    if not response.usage_metadata:
         raise RuntimeError("failed api request")
     print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count} ')
+    print("Response: ")
     print(response.text)
 
 
