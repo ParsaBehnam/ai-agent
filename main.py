dde5a81c87977be68701537468778c84394a15a9
ParsaBehnam <183300741+ParsaBehnam@users.noreply.github.com>
2026-03-26 21:50:51 +0300

    add --verbose flag to user_prompt

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from google.genai import types
 def main():
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action='store_true', help="Enable verbose output")
     args = parser.parse_args()
 
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
@@ -24,7 +25,9 @@ def main():
     
     if not response.usage_metadata:
         raise RuntimeError("failed api request")
-    print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count} ')
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count} ')
     print("Response: ")
     print(response.text)
 
